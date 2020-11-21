72878f2bb8c13c09e1fd5d833068fff93a99e215
Peter Greaney <petergreaney@gmail.com>
2020-11-21 21:08:30 +0700

    Update TheHeader.vue

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -20,17 +20,17 @@
             </div>
           </div>
           <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-            <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Product</a>
+            <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Tables</a>
 
-            <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Features</a>
+            <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Seating</a>
 
-            <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Marketplace</a>
+            <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Storage</a>
 
-            <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Company</a>
+            <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Objects</a>
 
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Log in</a>
+          
           </div>
         </nav></div>
-    <!-- <AppSearchInput class="ml-8" /> -->
+    <AppSearchInput class="ml-8" />
   </header>
 </template>
